8f25c720e103732667ad9acce88ab7e27533e31a
xulihang <xulihanghai@163.com>
2021-11-03 09:23:04 +0800

    ask for camera permission for android upon start

--- www/js/index.js
+++ www/js/index.js
@@ -25,6 +25,7 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
     //console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+    askForAndroidPermissions();
 }
 
 function GotoCustomScanner(){
@@ -44,107 +45,6 @@ function dirname(path)
    return path.replace(/\\/g,'/').replace(/\/[^\/]*$/, '');
 }
 
-var localStream, localVideoEl, localDeviceId;
-function TestGetUserMedia(deviceId) {
-
-    if (!deviceId) {
-        return navigator.mediaDevices.enumerateDevices().then(function(devices) {
-            var newDevice = devices.filter(function(device) {
-                return device.kind === 'videoinput';
-            }).find(function(device, idx) {
-                return device.deviceId !== 'default';
-            });
-
-            localDeviceId = newDevice ? newDevice.deviceId : null;
-            return TestGetUserMedia(localDeviceId || 'default');
-        });
-    }
-
-    console.debug('TestGetUserMedia', deviceId);
-    alert(deviceId);
-    return navigator.mediaDevices.getUserMedia({
-        /*
-        video: true,
-        */
-        video: {
-            deviceId: deviceId,
-
-            /*
-            width: {
-                max: 1280,
-                min: 640
-            },
-            height: {
-                max: 720,
-                min: 480
-            },
-            */
-                                               /*
-            height: {
-                ideal: 240,
-                min: 180,
-                max: 480
-            },
-                                                */
-            //facingMode: 'environment'
-            //height: 480,
-            //width: 1280,
-            //height: 720,
-            //width: 1280,
-            //height: 960,
-            //aspectRatio: 16/9,
-            //aspectRatio: 11/9,
-            //aspectRatio: 4/3,
-            //frameRate:{ min: 30.0, max: 30.0 }
-        },
-        audio: true
-        /*
-        video: {
-          // Test Back Camera
-          //deviceId: 'com.apple.avfoundation.avcapturedevice.built-in_video:0'
-          //sourceId: 'com.apple.avfoundation.avcapturedevice.built-in_video:0'
-          deviceId: {
-            exact: 'com.apple.avfoundation.avcapturedevice.built-in_video:0'
-          }
-        },
-        audio: {
-          deviceId: {
-            exact: 'Built-In Microphone'
-          }
-        }*/
-    }).then(function(stream) {
-
-        console.debug('getUserMedia.stream', stream);
-        console.debug('getUserMedia.stream.getTracks', stream.getTracks());
-
-        TestSetLocalStream(stream);
-
-        // Test mute at Start
-        /*
-        localStream.getAudioTracks().forEach(function (track) {
-            track.enabled = false;
-        });
-        */
-        return localStream;
-
-    }).catch(function(err) {
-        console.error('getUserMediaError', err, err.stack);
-        alert(err.stack);
-    });
-}
-
-
-function TestSetLocalStream(localStreamMedia) {
-    var appContainer = document.querySelector('.app');
-    localVideoEl = appContainer.querySelector('.local-video');
-
-    // Note: Expose for debug
-    localStream = localStreamMedia;
-
-
-    // Attach local stream to video element
-    localVideoEl.srcObject = localStream;
-}
 
 
 
