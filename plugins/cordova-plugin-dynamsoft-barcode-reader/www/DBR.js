ff09a1352b2476e4117005e789d3d356fa0f4238
xulihang <xulihanghai@163.com>
2021-11-10 18:06:42 +0800

    update gitignore

---
+++ plugins/cordova-plugin-dynamsoft-barcode-reader/www/DBR.js
@@ -0,0 +1,26 @@
+cordova.define("cordova-plugin-dynamsoft-barcode-reader.DBR", function(require, exports, module) { var exec = require('cordova/exec');
+
+exports.decode = function (arg0, success, error) {
+    exec(success, error, 'DBR', 'decode', [arg0]);
+};
+
+exports.init = function (arg0, success, error) {
+    if (!arg0){
+        arg0="200001";
+    }
+    exec(success, error, 'DBR', 'init', [arg0]);
+};
+
+exports.initWithOrganizationID = function (arg0, success, error) {
+    exec(success, error, 'DBR', 'initWithOrganizationID', [arg0]);
+};
+
+exports.initRuntimeSettingsWithString = function (arg0, success, error) {
+    exec(success, error, 'DBR', 'initRuntimeSettingsWithString', [arg0]);
+};
+
+exports.outputSettingsToString = function (success, error) {
+    exec(success, error, 'DBR', 'outputSettingsToString', []);
+};
+
+});
