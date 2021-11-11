abe72805bf39a212aa06139e64ed847c733843ad
xulihang <xulihanghai@163.com>
2021-11-11 10:20:53 +0800

    update gh-pages

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -34,9 +34,9 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-android-permissions": "1.1.2",
-    "cordova-plugin-iosrtc": "8.0.0",
     "cordova-plugin-whitelist": "1.3.5",
+    "cordova-plugin-iosrtc": "8.0.0",
+    "cordova-plugin-android-permissions": "1.1.2",
     "cordova-plugin-device": "2.0.3",
     "cordova-plugin-dynamsoft-barcode-reader": "0.0.1"
 }
